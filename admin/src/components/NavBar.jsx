c37164c808f64b7d8ce835c3790c69e4bae3cefa
Natalia Stan <popkids.ua@gmail.com>
2025-12-02 15:44:01 +0200

    Add NavBar and Add-page

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import assets from "../assets/assets";
 
-const NavBar = () => {
+const NavBar = ({ setToken }) => {
   return (
     <div className="flex item-center py-2 px-[4%] justify-between">
       <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
-      <button className="border-0 bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer">
+      <button
+        onClick={() => setToken("")}
+        className="border-0 bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer"
+      >
         Logout
       </button>
     </div>
